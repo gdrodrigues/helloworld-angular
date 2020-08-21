fa8ed6bd0268a35b53f3aabea6d4f69966ab8cca
gdrodrigues <gabrieldr@id.uff.br>
2020-08-21 18:31:26 -0300

    Home with cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,12 @@ export class AppComponent implements OnInit{
   itensPaginaFiltroResultadoPaginado: Array<ItemSubmenu> = new Array<ItemSubmenu>();
   itensDialog: Array<ItemSubmenu> = new Array<ItemSubmenu>();
   itensTable: Array<ItemSubmenu> = new Array<ItemSubmenu>();
-  componentes: Array<Componente> = new Array<Componente>();
+  //componentes: Array<Componente> = new Array<Componente>();
 
   ngOnInit() {
     this.addItensMenu();
     this.addItensSubMenu();
-    this.addComponentes();
+    //this.addComponentes();
   }
   addItensMenu() {
     this.itensMenu.push(new ItemMenu("Pagina Filtro Resultado Paginado", false, true, this.itensPaginaFiltroResultadoPaginado));
@@ -37,11 +37,19 @@ export class AppComponent implements OnInit{
     this.itensDialog.push(new ItemSubmenu("Dialog 1", false, true));
     this.itensTable.push(new ItemSubmenu("Tabela 10x10", false,true));
   }
-
+  /*
   addComponentes(){
-    this.componentes.push(new Componente("Dialog", "Esse componente é um dialog que faz isso, isso e isso",null,'./app/teste/teste.component.html'));
-    this.componentes.push(new Componente("File Upload", "Esse componente é um file upload que faz isso, isso e isso",null,'./app/teste/teste.component.html'));
-    this.componentes.push(new Componente("Button", "Esse componente é um button que faz isso, isso e isso",null,'./app/teste/teste.component.html'));
+    this.componentes.push(new Componente("Pagina Filtro Resultado Paginado", "Componente que encapsula toda a área de resultado de uma tela de filtro do Selic. O componente possui paginação para os resultados",null,null));
+    this.componentes.push(new Componente("Dialog", "Componente \"Diálogo\" com função alert e modal. Estão disponíveis em diferentes tamanhos.",null,'./app/teste/teste.component.html'));
+    this.componentes.push(new Componente("File Upload", "Componente para upload de arquivos",null,'./app/teste/teste.component.html'));
+    this.componentes.push(new Componente("Button", "Botões para utilização em formulário, tabela e para inclusão de itens em tabela. Disponíveis em versões \"habilitado\" e \"desabilitado\" ",null,null));
+    this.componentes.push(new Componente("Loader - Tela Cheia", "Componente de loader para a tela cheia", null, null));
+    this.componentes.push(new Componente("Loader - Componente", "Componente de loader para um componente específico", null, null));
+    this.componentes.push(new Componente("Tabela Simples", "Tabela com título para as colunas e sem scroll. Indicado caso a tabela não tenha muitas linhas.", null, null));
+    this.componentes.push(new Componente("Tabela Page", "Tabela com título, header fixo e scroll que ficará diretamente na página em que é utilizado.",null,null));
+    this.componentes.push(new Componente("Tabela Form", "Tabela com título, header fixo e scroll. Ela estará dentro de uma estrutura form", null, null));
+    this.componentes.push(new Componente("Tabela Dialog", "Tabela dentro de uma estrutura de dialog. Ela possui título, header fixo e scroll, além disso, estará dentro de uma estrutura form", null,null))
   }
+   */
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Componente} from "../componente";
 
 @Component({
@@ -8,12 +8,25 @@ import {Componente} from "../componente";
 })
 export class HomeComponent implements OnInit {
 
-  @Input()
-  componentes: Array<Componente>
+  componentes: Array<Componente> = new Array<Componente>();
 
   constructor() { }
 
   ngOnInit(): void {
+    this.addComponentes();
+  }
+
+  addComponentes(){
+    this.componentes.push(new Componente("Pagina Filtro Resultado Paginado", "Componente que encapsula toda a área de resultado de uma tela de filtro do Selic. O componente possui paginação para os resultados",null,null));
+    this.componentes.push(new Componente("Dialog", "Componente \"Diálogo\" com função alert e modal. Estão disponíveis em diferentes tamanhos.",null,'./app/teste/teste.component.html'));
+    this.componentes.push(new Componente("File Upload", "Componente para upload de arquivos",null,'./app/teste/teste.component.html'));
+    this.componentes.push(new Componente("Button", "Botões para utilização em formulário, tabela e para inclusão de itens em tabela. Disponíveis em versões \"habilitado\" e \"desabilitado\" ",null,null));
+    this.componentes.push(new Componente("Loader - Tela Cheia", "Componente de loader para a tela cheia", null, null));
+    this.componentes.push(new Componente("Loader - Componente", "Componente de loader para um componente específico", null, null));
+    this.componentes.push(new Componente("Tabela Simples", "Tabela com título para as colunas e sem scroll. Indicado caso a tabela não tenha muitas linhas.", null, null));
+    this.componentes.push(new Componente("Tabela Page", "Tabela com título, header fixo e scroll que ficará diretamente na página em que é utilizado.",null,null));
+    this.componentes.push(new Componente("Tabela Form", "Tabela com título, header fixo e scroll. Ela estará dentro de uma estrutura form.", null, null));
+    this.componentes.push(new Componente("Tabela Dialog", "Tabela dentro de uma estrutura de dialog. Ela possui título, header fixo e scroll, além disso, estará dentro de uma estrutura form.", null,null))
   }
 
 }
