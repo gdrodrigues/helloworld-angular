bdba592969bd8dc03c15f8890e1285adfb9c61cb
gdrodrigues <gabrieldr@id.uff.br>
2020-09-07 17:38:35 -0300

    menu with submenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,13 +22,12 @@ export class AppComponent implements OnInit{
     //this.addComponentes();
   }
   addItensMenu() {
-    this.itensMenu.push(new ItemMenu("Pagina Filtro Resultado Paginado", false, true, this.itensPaginaFiltroResultadoPaginado));
+    this.itensMenu.push(new ItemMenu("Filtro Resultado", false, true, this.itensPaginaFiltroResultadoPaginado));
     this.itensMenu.push(new ItemMenu("Dialog", false, true, this.itensDialog));
     this.itensMenu.push(new ItemMenu("File Upload", false, true, null));
     this.itensMenu.push(new ItemMenu("Button", false, true, null, 'teste'));
     this.itensMenu.push(new ItemMenu("Loader", false, true, null));
     this.itensMenu.push(new ItemMenu("Table", false, true, this.itensTable, 'teste2'));
-    this.itensMenu.push(new ItemMenu("FAQ", false,true, null));
   }
 
   addItensSubMenu(){
